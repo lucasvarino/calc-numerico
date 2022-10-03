3181b15f6193022d2abee9f787cef1537ce92b8f
dfreita4s <danifp19@gmail.com>
2022-10-03 17:47:46 -0300

    euler explicito pronto

--- Lista-1-pt1.rkt
+++ Lista-1-pt1.rkt
@@ -1,5 +1,8 @@
 #lang racket
 
+;; Graphics lib
+(require graphics/graphics)
+
 ; K constant
 (define K 0.035871952)
 ; Initial time
@@ -14,5 +17,15 @@
 (define step
   (/ (- t_f t_i) (- t_f 1)))
 ;; Exact solution 
-(define (exact-solution θ_0 θ_m K t)
+(define (exact-solution t)
   (+ (* (- θ_0 θ_m) (exp (* t (- K)))) θ_m))
+;; Explict Euler
+(define listOfResult (list θ_0))
+
+(define (explicit-euler t_f t_i listOfResult)
+  (if (< t_i t_f)
+      (explicit-euler t_f (+ 1 t_i) (append listOfResult (list
+                                                          (+ (* step (* (- K) (- (list-ref listOfResult t_i) θ_m))) (list-ref listOfResult t_i)))))
+      listOfResult))
+
+(explicit-euler t_f t_i listOfResult)
