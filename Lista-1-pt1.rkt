fab0a280070e059ec5ef8fb4f78ed97c2c64f7d9
dfreita4s <danifp19@gmail.com>
2022-10-03 20:50:36 -0300

    implicit pronto

--- Lista-1-pt1.rkt
+++ Lista-1-pt1.rkt
@@ -13,19 +13,43 @@
 (define θ_0 99)
 ; Ambient temperature
 (define θ_m 27)
-; Step
+;; Step
 (define step
   (/ (- t_f t_i) (- t_f 1)))
+
+;; Linspace
+(define linspace
+  (λ (start stop [num 50])
+    (range start stop (/ stop num))))
+; (linspace 0 50 50)
+
 ;; Exact solution 
-(define (exact-solution t)
-  (+ (* (- θ_0 θ_m) (exp (* t (- K)))) θ_m))
+(define (exact-solution t i result)
+  (if (< i (length t))
+         (exact-solution t (+ 1 i) (append result (list (+ (* (- θ_0 θ_m) (exp (* (list-ref t i) (- K)))) θ_m))))
+         result))
+; (exact-solution (linspace 0 50 50) 0 '())
+
 ;; Explict Euler
-(define listOfResult (list θ_0))
+(define listOfResultExp (list θ_0))
 
-(define (explicit-euler t_f t_i listOfResult)
+(define (explicit-euler t_f t_i listOfResultExp)
   (if (< t_i (- t_f 1))
-      (explicit-euler t_f (+ 1 t_i) (append listOfResult (list
-                                                          (+ (* step (* (- K) (- (list-ref listOfResult t_i) θ_m))) (list-ref listOfResult t_i)))))
-      listOfResult))
+      (explicit-euler t_f (+ 1 t_i) (append listOfResultExp (list
+                                                          (+ (* step (* (- K) (- (list-ref listOfResultExp t_i) θ_m))) (list-ref listOfResultExp t_i)))))
+      listOfResultExp))
 
-(explicit-euler t_f t_i listOfResult)
+;(explicit-euler t_f t_i listOfResultExp)
+
+
+;; Implicit Euler
+(define listOfResultImp (list θ_0))
+
+(define (implicit-euler t_f t_i listOfResultImp)
+  (if (< t_i (- t_f 1))
+      (implicit-euler t_f (+ 1 t_i) (append listOfResultImp (list
+                                                             (/ (+ (list-ref listOfResultImp t_i) (* K step θ_m)) (+ 1 (* K step))))))
+      listOfResultImp))
+
+;(implicit-euler t_f t_i listOfResultExp)
+
