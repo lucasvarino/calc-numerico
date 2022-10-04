b40e2fa06ed97720e5efa30522b9488bf710fcd0
lucasvarino <lucasvarino@gmail.com>
2022-10-03 23:44:19 -0300

    Tabela agora

--- Lista-1-pt1.rkt
+++ Lista-1-pt1.rkt
@@ -1,7 +1,11 @@
 #lang racket
 
+;; Alunos: Daniel Freitas e Lucas de Oliveira Varino
+
 ;; Graphics lib
 (require graphics/graphics)
+(require text-table)
+(require text-table/utils)
 
 ; K constant
 (define K 0.035871952)
@@ -53,3 +57,45 @@
 
 ;(implicit-euler t_f t_i listOfResultExp)
 
+;; Crank-Nicolson
+(define listOfResultCN (list θ_0))
+
+(define (crank-nicolson t_f t_i listOfResultCN)
+  (if (< t_i (- t_f 1))
+      (crank-nicolson t_f (+ 1 t_i) (append listOfResultCN (list
+                                                             (/ (+ (/ (* (- K) step (- (list-ref listOfResultCN t_i) (* 2 θ_m))) 2) (list-ref listOfResultCN t_i)) (+ 1 (/ (* K step) 2))))))
+      listOfResultCN))
+
+;(crank-nicolson t_f t_i listOfResultCN)
+
+;; Tabelas
+
+(print-table
+   #:->string (list ~a ; 1
+                    (~r*) ; 2
+                    (~r* #:notation 'exponential) ; 3
+                    (~r* #:precision '(= 2)) ; 4 (good)
+                    (~r* #:notation 'exponential #:precision '(= 2)) ; 5 (good)
+                    (~r* #:min-width 10 #:pad-string ".")) ; 6
+   #:align '(right ...)
+   #:row-sep? '(#f #t #f ...)
+   (cons
+    '("1" "2" "3" "4 (good)" "5 (good)" "6")
+    (transpose
+    (make-list 6 `(header 1111.11 22.222 3333000.0 4440000000000.0 ,(sqrt 2))))))
+
+          
+
+;; Errors
+
+(define (findError exact aprox result)
+  (if (< (length result) (length aprox))
+      (findError exact aprox (append result (list (abs (- (list-ref exact (length result)) (list-ref aprox (length result)))))))
+      (apply max result)))
+
+(display "Erro para o metodo Euler Explicito: ")
+(findError (exact-solution (linspace 0 51 50) 0 '()) (explicit-euler t_f t_i listOfResultExp) '())
+(display "Erro para o metodo Euler Implicito: ")
+(findError (exact-solution (linspace 0 51 50) 0 '()) (implicit-euler t_f t_i listOfResultImp) '())
+(display "Erro para o metodo Crank Nicolson: ")
+(findError (exact-solution (linspace 0 51 50) 0 '()) (crank-nicolson t_f t_i listOfResultCN) '())
