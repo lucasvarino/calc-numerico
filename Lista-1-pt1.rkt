2cc718570bc6fd17392f39cc7ac04282edaeb7ce
lucasvarino <lucasvarino@gmail.com>
2022-10-04 19:14:48 -0300

    Lista 01 - Parte 1 feita

--- Lista-1-pt1.rkt
+++ Lista-1-pt1.rkt
@@ -2,10 +2,28 @@
 
 ;; Alunos: Daniel Freitas e Lucas de Oliveira Varino
 
-;; Graphics lib
+;; Libs
 (require graphics/graphics)
 (require text-table)
 (require text-table/utils)
+(require math/bigfloat)
+
+;; Precision
+; 64-bits
+(bf-precision 64)
+#|
+Erro para o metodo Euler Explicito: 0.5029193064673478
+Erro para o metodo Euler Implicito: 0.4668773416941292
+Erro para o metodo Crank Nicolson:  0.013553951180689694
+|#
+; 32-bits
+;(bf-precision 32)
+#|
+Erro para o metodo Euler Explicito: 0.5029192864894867
+Erro para o metodo Euler Implicito: 0.4668773114681244
+Erro para o metodo Crank Nicolson:  0.0135539472103118
+|#
+
 
 ; K constant
 (define K 0.035871952)
@@ -19,19 +37,20 @@
 (define θ_m 27)
 ;; Step
 (define step
-  (/ (- t_f t_i) (- t_f 1)))
+  (bigfloat->flonum (bf (/ (- t_f t_i) (- t_f 1)))))
 
 ;; Linspace
 (define linspace
   (λ (start stop [num 50])
-    (range start stop (/ stop num))))
+    (range start stop (bigfloat->flonum (bf (/ stop num))))))
 ; (linspace 0 50 50)
 
 ;; Exact solution 
 (define (exact-solution t i result)
   (if (< i (length t))
-         (exact-solution t (+ 1 i) (append result (list (+ (* (- θ_0 θ_m) (exp (* (list-ref t i) (- K)))) θ_m))))
-         result))
+      (exact-solution t (+ 1 i) (append result (list 
+                                                (bigfloat->flonum (bf (+ (* (- θ_0 θ_m) (exp (* (list-ref t i) (- K)))) θ_m))))))
+      result))
 ; (exact-solution (linspace 0 50 50) 0 '())
 
 ;; Explict Euler
@@ -40,7 +59,7 @@
 (define (explicit-euler t_f t_i listOfResultExp)
   (if (< t_i (- t_f 1))
       (explicit-euler t_f (+ 1 t_i) (append listOfResultExp (list
-                                                          (+ (* step (* (- K) (- (list-ref listOfResultExp t_i) θ_m))) (list-ref listOfResultExp t_i)))))
+                                                             (bigfloat->flonum (bf (+ (* step (* (- K) (- (list-ref listOfResultExp t_i) θ_m))) (list-ref listOfResultExp t_i)))))))
       listOfResultExp))
 
 ;(explicit-euler t_f t_i listOfResultExp)
@@ -52,7 +71,7 @@
 (define (implicit-euler t_f t_i listOfResultImp)
   (if (< t_i (- t_f 1))
       (implicit-euler t_f (+ 1 t_i) (append listOfResultImp (list
-                                                             (/ (+ (list-ref listOfResultImp t_i) (* K step θ_m)) (+ 1 (* K step))))))
+                                                             (bigfloat->flonum (bf (/ (+ (list-ref listOfResultImp t_i) (* K step θ_m)) (+ 1 (* K step))))))))
       listOfResultImp))
 
 ;(implicit-euler t_f t_i listOfResultExp)
@@ -63,39 +82,36 @@
 (define (crank-nicolson t_f t_i listOfResultCN)
   (if (< t_i (- t_f 1))
       (crank-nicolson t_f (+ 1 t_i) (append listOfResultCN (list
-                                                             (/ (+ (/ (* (- K) step (- (list-ref listOfResultCN t_i) (* 2 θ_m))) 2) (list-ref listOfResultCN t_i)) (+ 1 (/ (* K step) 2))))))
+                                                            (bigfloat->flonum (bf (/ (+ (/ (* (- K) step (- (list-ref listOfResultCN t_i) (* 2 θ_m))) 2) (list-ref listOfResultCN t_i)) (+ 1 (/ (* K step) 2))))))))
       listOfResultCN))
 
 ;(crank-nicolson t_f t_i listOfResultCN)
 
-;; Tabelas
-
+;; Tables
+(display "\n======\nTabela Comparação de metodos\n======\n")
 (print-table
-   #:->string (list ~a ; 1
-                    (~r*) ; 2
-                    (~r* #:notation 'exponential) ; 3
-                    (~r* #:precision '(= 2)) ; 4 (good)
-                    (~r* #:notation 'exponential #:precision '(= 2)) ; 5 (good)
-                    (~r* #:min-width 10 #:pad-string ".")) ; 6
-   #:align '(right ...)
-   #:row-sep? '(#f #t #f ...)
-   (cons
-    '("1" "2" "3" "4 (good)" "5 (good)" "6")
-    (transpose
-    (make-list 6 `(header 1111.11 22.222 3333000.0 4440000000000.0 ,(sqrt 2))))))
-
-          
+ #:->string (list (~r* #:precision '(= 10)))
+ #:align '(center ...)
+ #:row-sep? #t
+ #:border-style 'double ;'latex ;<- Se quiser que saia em latex 
+ (cons
+  '("Exato" "Euler Explícito" "Euler Implícito" "Crank Nicolson")
+  (transpose
+   (list (exact-solution (linspace 0 51 50) 0 '())
+    (explicit-euler t_f t_i listOfResultExp) 
+    (implicit-euler t_f t_i listOfResultImp)
+    (crank-nicolson t_f t_i listOfResultCN)))))
 
 ;; Errors
 
 (define (findError exact aprox result)
   (if (< (length result) (length aprox))
-      (findError exact aprox (append result (list (abs (- (list-ref exact (length result)) (list-ref aprox (length result)))))))
+      (findError exact aprox (append result (list (abs (bigfloat->flonum (bf (- (list-ref exact (length result)) (list-ref aprox (length result)))))))))
       (apply max result)))
 
 (display "Erro para o metodo Euler Explicito: ")
-(findError (exact-solution (linspace 0 51 50) 0 '()) (explicit-euler t_f t_i listOfResultExp) '())
+(bigfloat->flonum (bf (findError (exact-solution (linspace 0 51 50) 0 '()) (explicit-euler t_f t_i listOfResultExp) '())))
 (display "Erro para o metodo Euler Implicito: ")
-(findError (exact-solution (linspace 0 51 50) 0 '()) (implicit-euler t_f t_i listOfResultImp) '())
-(display "Erro para o metodo Crank Nicolson: ")
-(findError (exact-solution (linspace 0 51 50) 0 '()) (crank-nicolson t_f t_i listOfResultCN) '())
+(bigfloat->flonum (bf (findError (exact-solution (linspace 0 51 50) 0 '()) (implicit-euler t_f t_i listOfResultImp) '())))
+(display "Erro para o metodo Crank Nicolson:  ")
+(bigfloat->flonum (bf (findError (exact-solution (linspace 0 51 50) 0 '()) (crank-nicolson t_f t_i listOfResultCN) '())))
